60ed46937bff09723fef239ee5df743cc06f2326
Jaime Mejia <jaimemejia1003@gmail.com>
2025-09-14 12:26:17 -0500

    first commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const message = "Hello World";
+</script>
+
+<template>
+    <div>
+        <NuxtRouteAnnouncer />
+        <NuxtPage />
+        <p>{{ message }}</p>
+    </div>
+</template>
